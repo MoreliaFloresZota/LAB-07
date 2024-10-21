5bde55bbe536f8ea39c238dc7217ced3b1f7e67d
MoreliaFloresZota <moreliana.27.09.2000@gmail.com>
2024-10-20 20:55:50 -0400

    first commit

---
+++ vite-project/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import { usesState, useState} from "react";
+
+export const Button = props; => {
+
+    const { text } = props;      'text' is mi
+    const[count, setCount] = useState(0);
+
+    function handlerButton() {
+        setCount(count + 1);
+    }
+
+    return (
+        <button onClick={handlerButton}>
+              {text} { count }
+        </button>
+    );
+} 
